89ce98f9b1d6d5f7bb7d6fe9debb49ea49212f37
subru-37 <easwaransubramaniOO7@gmail.com>
2022-12-03 18:25:03 +0530

    Updates

---
+++ precache-manifest.fb323111288311016628b6d0cec1af47.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a421538bc97104d54bea2882f6464eb8",
+    "url": "/TO-DO-app/index.html"
+  },
+  {
+    "revision": "8f0fad80f2d9fcdc813d",
+    "url": "/TO-DO-app/static/js/2.1a7f43d7.chunk.js"
+  },
+  {
+    "revision": "15e338928da5ac370059",
+    "url": "/TO-DO-app/static/js/main.cbdbd042.chunk.js"
+  },
+  {
+    "revision": "fb2f8a2275dc2eeb6955",
+    "url": "/TO-DO-app/static/js/runtime-main.bc00e64a.js"
+  }
+]);
